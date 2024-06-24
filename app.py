1dabe437107ccc214cd23963cb10cb6eed5cb70d
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-06-24 17:03:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from modelos.jurado import Jurado
 from modelos.aluno import Aluno
 
-caua = Aluno('Caua', 'TI', 'Mister')
-jurado = Jurado('Carléia', 'Educacional')
 
-jurado.atribuir_nota(caua, 5, 5, 5, 5)
-jurado.atribuir_nota(caua, 6, 6, 6, 6)
+def main():
+    caua = Aluno('Caua', 'TI', 'Mister')
+    jurado = Jurado('Carléia', 'Educacional')
 
-print(caua)
+    jurado.atribuir_nota(caua, 5, 5, 5, 5)
+    jurado.atribuir_nota(caua, 6, 6, 6, 6)
+
+    print(caua)
+
+if __name__ == "__main__":
+    main()
